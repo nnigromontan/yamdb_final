96b494079f2289deb339cc61157454c49cff6fcf
Александр Богус <nigromontan92@gmail.com>
2022-12-05 12:34:00 +0300

    3.4 gunicorn fixed

--- api_yamdb/api/utils.py
+++ api_yamdb/api/utils.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 
 def check_username_not_me(value):
-    """Валидатор username."""
+    """Валидатор параметра username."""
     if value.lower() == 'me':
         raise serializers.ValidationError(
             'Нельзя назвать пользователя "me"!'
